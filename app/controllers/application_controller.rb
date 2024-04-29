ee6757eb9b7df6c51b8a54370124318328b1bc6d
Mauricio Gravena <mauricio.gravena@rebase.com.br>
2024-04-29 11:41:15 -0300

    remove Bearer from the token string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::API
 
   def authenticate
     authenticate_or_request_with_http_token do |token, _options|
-      JWT.decode(token, ENV['SECRET_KEY'], true, algorithm: 'HS256')
+      JWT.decode(
+        token.sub(/^Bearer\s+/, ''), ENV['SECRET_KEY'], true, algorithm: 'HS256'
+      )
     rescue JWT::DecodeError
       head :unprocessable_entity
     end
